eb6d31986deaea0dde1d57ceb507948645406539
Charles Offenbacher <charles.offenbacher@gmail.com>
2013-03-01 12:41:46 -0800

    first scraper pass

--- scraper/scraper/items.py
+++ scraper/scraper/items.py
@@ -8,4 +8,6 @@ from scrapy.item import Item, Field
 class ScraperItem(Item):
     # define the fields for your item here like:
     # name = Field()
-    pass
+    title = Field()
+    link = Field()
+    desc = Field()
